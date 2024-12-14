134f5909df58c1a889d8b44f3a0b597131ddbd4a
Katarinawallenhoff <katarina.wallenhoff@outlook.com>
2024-12-14 17:50:18 +0100

    Find distance
    This function is made for finding the distances and then giving value of the Id's and distances to variables

--- index.js
+++ index.js
@@ -64,6 +64,48 @@ function createBoxes() {
  
  const cityId = findCityId(userInput);
 
+ function findFurthestAndClosestCity(cityId) {
+    let furthestDistance = 0;
+    let closestDistance = 10000;
+    let furthestCity = null;
+    let closestCity = null;
+    let closestCityId;
+    let furthestCityId;
+ 
+ 
+    for (let distance of distances) {
+        if (cityId === distance.city1 || cityId === distance.city2) {
+            if (distance.distance > furthestDistance) {
+                furthestDistance = distance.distance;
+                furthestCity = distance;
+            }
+            if (distance.distance < closestDistance) {
+                closestDistance = distance.distance;
+                closestCity = distance;
+            }
+        }
+    }
+ 
+ 
+    if (closestCity) {
+        closestCityId = (cityId === closestCity.city1) ? closestCity.city2 : closestCity.city1;
+    }
+ 
+ 
+    if (furthestCity) {
+        furthestCityId = (cityId === furthestCity.city1) ? furthestCity.city2 : furthestCity.city1;
+    }
+ 
+ 
+    return {
+        furthestCity,
+        closestCity,
+        closestCityId,
+        furthestCityId
+    };
+ }
+ 
+
 
 
 
