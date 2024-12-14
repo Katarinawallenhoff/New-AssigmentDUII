36b6a2328ee29c485ea6f895c3a95acbb2357541
Katarinawallenhoff <katarina.wallenhoff@outlook.com>
2024-12-14 17:36:26 +0100

    First step
    References to the elements, a function that brings out every name in the cities array and places them in a separate array (cityArray)

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+"use strict"
 
+
+const h2Element = document.querySelector("h2");
+const cityNameTable = document.querySelector("#cities");
+const h3Element = document.querySelector("h3");
+
+
+let userInput = prompt("Skriv in en stad");
+
+
+function cityNames() {
+   let cityNameArray = [];
+   for (let city of cities) {
+       cityNameArray.push(city.name);
+   }
+   return cityNameArray;
+}
+
+
+let cityArray = cityNames();
 
 
 
